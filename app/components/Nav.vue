b605c6251cbf6f6c07a069106ad76fdb85efee5d
doniahusien <doniahusien@gmail.com>
2025-12-18 01:03:08 +0200

    enhance nav

--- app/components/Nav.vue
+++ app/components/Nav.vue
@@ -1,16 +1,18 @@
-<script setup>
+<script setup lang="ts">
 const isSticky = ref(false);
 const showMega = ref(false);
 const showservices = ref(false);
 const toggle = ref(false);
 
-const { locale, t, setLocale } = useI18n();
+const { locale, t } = useI18n();
 const switchLocalePath = useSwitchLocalePath();
 const targetLocale = computed(() => (locale.value === "ar" ? "en" : "ar"));
 const switchPath = computed(() => switchLocalePath(targetLocale.value));
 
-const isRTL = computed(() => locale.value === 'ar')
-const localePath= useLocalePath();
+const isRTL = computed(() => locale.value === "ar");
+const localePath = useLocalePath();
+const route = useRoute();
+
 const mega = ref([
   "ABOUT",
   "PROJECT_LIST",
@@ -36,96 +38,176 @@ const mega = ref([
 ]);
 
 const handleScroll = () => {
-  if (window.scrollY > 50) {
-    isSticky.value = true;
-  } else {
-    isSticky.value = false;
-  }
+  isSticky.value = window.scrollY > 50;
 };
 
-onMounted(() => {
-  window.addEventListener("scroll", handleScroll);
-});
+onMounted(() => window.addEventListener("scroll", handleScroll));
+onUnmounted(() => window.removeEventListener("scroll", handleScroll));
 
-onUnmounted(() => {
-  window.removeEventListener("scroll", handleScroll);
-});
+const isActive = (path: string) =>
+  route.path === localePath(path) ? "text-secondary font-semibold" : "";
 </script>
+
 <template>
   <nav
     :class="[
-      'bg-white fixed p-4 w-full  shadow-md transition-all duration-300 z-50',
-      isSticky ? 'fixed ' : 'relative top-12 md:top-14',
+      'bg-white fixed w-full shadow-md transition-all duration-300 z-50 p-4',
+      isSticky ? 'top-0' : 'relative top-12 md:top-14',
     ]"
   >
     <div class="max-w-7xl mx-auto flex items-center justify-between md:px-6">
-      <NuxtImg src="/logo2x.png" class="object-contain w-32 md:w-48" alt="logo" />
-      <ul class="hidden md:flex items-center gap-8 text-base font-light">
-        <li><NuxtLink class="hover:text-secondary" :to="localePath('/')">{{$t('TITLES.HOME')}}</NuxtLink></li>
-        <li><NuxtLink class="hover:text-secondary" :to="localePath('/about')">{{$t('TITLES.ABOUT_US')}}</NuxtLink></li>
+      <NuxtImg src="/logo2x.png" class="object-contain w-30 md:w-40 lg:w-48" alt="logo" />
+
+      <ul class="hidden md:flex items-center gap-3 lg:gap-8 text-sm lg:text-base font-light">
+        <li>
+          <NuxtLink
+            :to="localePath('/')"
+            :class="[
+              isActive('/'),
+              'hover:text-secondary transition-colors duration-300',
+            ]"
+          >
+            {{ $t("TITLES.HOME") }}
+          </NuxtLink>
+        </li>
+        <li>
+          <NuxtLink
+            :to="localePath('/about')"
+            :class="[
+              isActive('/about'),
+              'hover:text-secondary transition-colors duration-300',
+            ]"
+          >
+            {{ $t("TITLES.ABOUT_US") }}
+          </NuxtLink>
+        </li>
+
         <li
           class="relative"
           @mouseenter="showservices = true"
           @mouseleave="showservices = false"
         >
-          <button class="hover:text-secondary">{{$t('TITLES.OUR_SERVICES')}} <span>▾</span></button>
-          <div
-            v-if="showservices"
-            @mouseenter="showservices = true"
-            class="absolute top-full w-66 text-base py-2 px-6 text-white"
-          >
-            <ul class="space-y-3 bg-secondary mt-5 p-5">
-              <li><NuxtLink :to="localePath('/services/land-transport')">{{$t('TITLES.LAND_TRANSPORT')}}</NuxtLink></li>
-              <li><NuxtLink :to="localePath('/services/warehousing')">{{$t('TITLES.WAREHOUSING')}}</NuxtLink></li>
-              <li><NuxtLink :to="localePath('/services/fast-delivery')">{{$t('TITLES.FAST_DELIVERY')}}</NuxtLink></li>
-              <li><NuxtLink :to="localePath('/services/worldwide-transport')">{{$t('TITLES.WORLDWIDE_TRANSPORT')}}</NuxtLink></li>
-              <li><NuxtLink :to="localePath('/services/sea-freight')">{{$t('TITLES.SEA_FREIGHT')}}</NuxtLink></li>
-              <li><NuxtLink :to="localePath('/services/reliable')">{{$t('TITLES.RELIABLE')}}</NuxtLink></li>
-            </ul>
-          </div>
-        </li>
-        <li><NuxtLink class="hover:text-secondary" :to="localePath('/blog')">{{$t('TITLES.BLOG')}}</NuxtLink></li>
-
-        <li class="relative" @mouseenter="showMega = true" @mouseleave="showMega = false">
-          <button class="hover:text-secondary flex items-center gap-1">
-           {{$t('TITLES.PAGES')}}
-            <span>▾</span>
+          <button class="hover:text-secondary">
+            {{ $t("TITLES.OUR_SERVICES") }} <span>▾</span>
           </button>
 
-          <div
-            v-show="showMega"
-            @mouseenter="showMega = true"
-            :class="[
-  isRTL ? 'translate-x-2/3' : '-translate-x-2/3'
-]"
-            class="absolute top-full min-w-7xl mx-auto text-base py-2 text-white"
+          <Transition
+            enter-active-class="transition ease-out duration-300"
+            enter-from-class="opacity-0 translate-y-2"
+            enter-to-class="opacity-100 translate-y-0"
+            leave-active-class="transition ease-in duration-200"
+            leave-from-class="opacity-100 translate-y-0"
+            leave-to-class="opacity-0 translate-y-2"
           >
-            <div class="grid grid-cols-4 w-full bg-secondary mt-5 gap-5">
-              <div class="col-span-1">
-                <NuxtImg src="/images/menubg.jpeg" alt="menu" class="w-full h-full" />
-              </div>
-              <ul class="col-span-3 py-10 grid grid-cols-3 gap-4 text-sm font-medium">
-                <li v-for="item in mega">
-                  <Icon :name="isRTL ? 'simple-line-icons:arrow-left' : 'weui:arrow-outlined'" class="text-white align-middle" />
-                  <NuxtLink :to="localePath(`/pages/${item}`)" class="align-middle ml-1 uppercase"> {{ $t(`TITLES.${item}`) }}</NuxtLink>
+            <div
+              v-if="showservices"
+              class="absolute top-full w-66 text-base py-2 px-6 text-white"
+            >
+              <ul class="space-y-3 bg-secondary mt-5 p-5">
+                <li
+                  v-for="item in [
+                    'LAND_TRANSPORT',
+                    'WAREHOUSING',
+                    'FAST_DELIVERY',
+                    'WORLDWIDE_TRANSPORT',
+                    'SEA_FREIGHT',
+                    'RELIABLE',
+                  ]"
+                  :key="item"
+                >
+                  <NuxtLink
+                    :to="localePath(`/services/${item}`)"
+                    :class="[isActive(`/services/${item}`), 'text-white font-light']"
+                  >
+                    {{ $t(`TITLES.${item.toUpperCase()}`) }}
+                  </NuxtLink>
                 </li>
               </ul>
             </div>
-          </div>
+          </Transition>
         </li>
 
-        <li><NuxtLink class="hover:text-red-500" :to="localePath('/contact')">{{$t('TITLES.CONTACT')}}</NuxtLink></li>
         <li>
           <NuxtLink
-            class="border font-semibold border-red-500 px-4 py-4 rounded-lg text-secondary"
-           :to="localePath('/get-quote')"
-            >{{$t("TITLES.GET_QUOTE")}}</NuxtLink
+            :to="localePath('/blog')"
+            :class="[
+              isActive('/blog'),
+              'hover:text-secondary transition-colors duration-300',
+            ]"
           >
-        </li>
-            <NuxtLink :to="switchPath" class="hidden lg:flex items-center text-secondary">
-            <Icon name="fluent:globe-48-filled" class="size-5" />
-            <span> {{ $t(`locale.${locale}`) }}</span>
+            {{ $t("TITLES.BLOG") }}
           </NuxtLink>
+        </li>
+
+        <li class="relative" @mouseenter="showMega = true" @mouseleave="showMega = false">
+          <button class="hover:text-secondary flex items-center gap-1">
+            {{ $t("TITLES.PAGES") }} <span>▾</span>
+          </button>
+          <Transition
+            enter-active-class="transition ease-out duration-500"
+            enter-from-class="opacity-0 translate-y-2"
+            enter-to-class="opacity-100 translate-y-0"
+            leave-active-class="transition ease-in duration-400"
+            leave-from-class="opacity-100 translate-y-0"
+            leave-to-class="opacity-0 translate-y-2"
+          >
+            <div
+              v-show="showMega"
+              :class="[isRTL ? 'translate-x-2/3' : '-translate-x-2/3']"
+              class="absolute top-full min-w-7xl mx-auto text-base py-2 text-white"
+            >
+              <div class="grid grid-cols-4 w-full bg-secondary mt-5 gap-5">
+                <div class="col-span-1">
+                  <NuxtImg src="/images/menubg.jpeg" alt="menu" class="w-full h-full" />
+                </div>
+                <ul class="col-span-3 py-10 grid grid-cols-3 gap-4 text-sm font-medium">
+                  <li v-for="item in mega" :key="item">
+                    <Icon
+                      :name="
+                        isRTL ? 'simple-line-icons:arrow-left' : 'weui:arrow-outlined'
+                      "
+                      class="text-white align-middle"
+                    />
+                    <NuxtLink
+                      :to="localePath(`/pages/${item}`)"
+                      :class="[
+                        isActive(`/pages/${item}`),
+                        'align-middle ml-1 uppercase font-light',
+                      ]"
+                    >
+                      {{ $t(`TITLES.${item}`) }}
+                    </NuxtLink>
+                  </li>
+                </ul>
+              </div>
+            </div>
+          </Transition>
+        </li>
+
+        <li>
+          <NuxtLink
+            :to="localePath('/contact')"
+            :class="[
+              isActive('/contact'),
+              'hover:text-secondary transition-colors duration-300',
+            ]"
+          >
+            {{ $t("TITLES.CONTACT") }}
+          </NuxtLink>
+        </li>
+        <li>
+          <NuxtLink
+            :to="localePath('/get-quote')"
+            class="border font-semibold border-secondary p-2 md:p-4 rounded-lg text-secondary"
+          >
+            {{ $t("TITLES.GET_QUOTE") }}
+          </NuxtLink>
+        </li>
+
+        <NuxtLink :to="switchPath" class="hidden lg:flex items-center text-secondary">
+          <Icon name="fluent:globe-48-filled" class="size-5" />
+          <span>{{ $t(`locale.${locale}`) }}</span>
+        </NuxtLink>
       </ul>
 
       <button
@@ -134,58 +216,85 @@ onUnmounted(() => {
       >
         <Icon name="basil:menu-solid" class="text-black/30 size-8" />
       </button>
+
       <div
-        v-if="toggle == true"
+        v-if="toggle"
         class="bg-white absolute top-full h-86 left-0 py-5 overflow-y-auto menu w-full text-center"
       >
         <ul class="space-y-6 text-sm">
-          <li class="border-b p-2 border-gray-100">
-            <NuxtLink class="hover:text-secondary" :to="localePath('/')">{{$t('TITLES.HOME')}}</NuxtLink>
-          </li>
-          <li class="border-b p-2 border-gray-100">
-            <NuxtLink class="hover:text-secondary" :to="localePath('/about')">{{$t('TITLES.ABOUT_US')}}</NuxtLink>
-          </li>
-              <li class="border-b p-2 border-gray-100"> 
-           {{$t('TITLES.OUR_SERVICES')}}
-          </li>
-          <li class="border-b p-2 border-gray-100">
-            <NuxtLink class="hover:text-secondary font-light" :to="localePath('/services/land-transport')">
-                       {{$t('TITLES.LAND_TRANSPORT')}}
+          <li
+            v-for="link in [
+              { name: 'HOME', path: '/' },
+              { name: 'ABOUT_US', path: '/about' },
+              { name: 'BLOG', path: '/blog' },
+              { name: 'CONTACT', path: '/contact' },
+            ]"
+            :key="link.path"
+            class="border-b p-2 border-gray-100"
+          >
+            <NuxtLink
+              :to="localePath(link.path)"
+              :class="[
+                isActive(link.path),
+                'hover:text-secondary transition-colors duration-300',
+              ]"
+            >
+              {{ $t(`TITLES.${link.name}`) }}
             </NuxtLink>
           </li>
-          <li class="border-b p-2 border-gray-100">
-            <NuxtLink class="hover:text-secondary font-light" :to="localePath('/services/fast-delivery')">           {{$t('TITLES.FAST_DELIVERY')}}</NuxtLink>
-          </li>
-          <li class="border-b p-2 border-gray-100">
-            <NuxtLink class="hover:text-secondary font-light" :to="localePath('/services/worldwide-transport')">           {{$t('TITLES.WORLDWIDE_TRANSPORT')}}</NuxtLink>
-          </li>
-          <li class="border-b p-2 border-gray-100">
-            <NuxtLink class="hover:text-secondary font-light" :to="localePath('/services/warehousing')">           {{$t('TITLES.WAREHOUSING')}}</NuxtLink>
-          </li>
-          <li class="border-b p-2 border-gray-100">
-            <NuxtLink class="hover:text-secondary font-light" :to="localePath('/services/sea-freight')">           {{$t('TITLES.SEA_FREIGHT')}}</NuxtLink>
-          </li>
-          <li class="border-b p-2 border-gray-100 font-light">
-            <NuxtLink class="hover:text-secondary" :to="localePath('/services/reliable')">           {{$t('TITLES.RELIABLE')}}</NuxtLink>
-          </li>
-          <li class="border-b p-2 border-gray-100">
-            <NuxtLink class="hover:text-secondary"  :to="localePath('/blog')">           {{$t('TITLES.BLOG')}}</NuxtLink>
-          </li>
-          <li class="border-b p-2 border-gray-100"> 
-           {{$t('TITLES.PAGES')}}          </li>
 
-          <li class="border-b p-2 border-gray-100" v-for="item in mega">
-            <NuxtLink :to="localePath(`/pages/${item}`)" class="align-middle ml-1 uppercase font-light"> {{ $t(`TITLES.${item}`) }}</NuxtLink>
-          </li>
           <li class="border-b p-2 border-gray-100">
-            <NuxtLink class="hover:text-secondary" :to="localePath('/contact')">           {{$t('TITLES.CONTACT')}}</NuxtLink>
+            {{ $t("TITLES.OUR_SERVICES") }}
+            <ul class="mt-2">
+              <li
+                v-for="item in [
+                  'land-transport',
+                  'warehousing',
+                  'fast-delivery',
+                  'worldwide-transport',
+                  'sea-freight',
+                  'reliable',
+                ]"
+                :key="item"
+                class="border-b p-2 border-gray-100"
+              >
+                <NuxtLink
+                  :to="localePath(`/services/${item}`)"
+                  :class="[
+                    isActive(`/services/${item}`),
+                    'hover:text-secondary font-light transition-colors duration-300',
+                  ]"
+                >
+                  {{ $t(`TITLES.${item.toUpperCase()}`) }}
+                </NuxtLink>
+              </li>
+            </ul>
           </li>
+
+          <li class="border-b p-2 border-gray-100">
+            {{ $t("TITLES.PAGES") }}
+            <ul class="mt-2">
+              <li v-for="item in mega" :key="item" class="border-b p-2 border-gray-100">
+                <NuxtLink
+                  :to="localePath(`/pages/${item}`)"
+                  :class="[
+                    isActive(`/pages/${item}`),
+                    'hover:text-secondary font-light transition-colors duration-300',
+                  ]"
+                >
+                  {{ $t(`TITLES.${item}`) }}
+                </NuxtLink>
+              </li>
+            </ul>
+          </li>
+
           <li class="border-b p-2 border-gray-100">
             <NuxtLink
-              class="border font-semibold border-secondary p-4 rounded-lg text-secondary"
               :to="localePath('/get-quote')"
-              >           {{$t('TITLES.GET_QUOTE')}}</NuxtLink
+              class="border font-semibold border-secondary px-4 py-4 rounded-lg text-secondary"
             >
+              {{ $t("TITLES.GET_QUOTE") }}
+            </NuxtLink>
           </li>
         </ul>
       </div>
--- nuxt.config.ts
+++ nuxt.config.ts
@@ -19,12 +19,14 @@ export default defineNuxtConfig({
             meta: [
                 { charset: 'utf-8' },
                 { name: 'viewport', content: 'width=device-width, initial-scale=1' },
-
                 {
                     name: 'robots',
                     content: `index,follow`,
                 },
             ],
+             link: [
+      { rel: 'icon', type: 'image/png', href: '/logo2x.png' } 
+    ]
     },
   },
 
