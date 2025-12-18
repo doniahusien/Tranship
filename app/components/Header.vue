882e5e9c9f85894e40172a12501f35931ad4ae7a
doniahusien <doniahusien@gmail.com>
2025-12-18 17:29:45 +0200

    refactor header

--- app/components/Header.vue
+++ app/components/Header.vue
@@ -1,42 +1,84 @@
 <template>
-  <header class=" bg-dark-white p-2 md:p-4 font-light text-text fixed  w-full text-sm md:text-base">
+  <header
+    class="bg-dark-white p-2 md:p-4 font-light text-text fixed w-full text-sm md:text-base"
+  >
     <div class="max-w-7xl mx-auto flex items-center md:justify-between md:px-6">
-    <ul class="flex gap-5">
-      <li class="flex items-center justify-center gap-1 ">
-        <Icon name="lineicons:phone" class="text-secondary font-semibold" />
-        <span  >1-234-567-8000</span>
-      </li>
-      <li class=" hidden md:flex items-center justify-center gap-1">
-        <Icon name="mdi-light:email" class="text-secondary font-semibold" />
-        <span >tranship@mail.com</span>
-      </li>
-      <li class="hidden md:flex items-center justify-center gap-1">
-        <Icon name="mdi-light:clock" class="text-secondary font-semibold" />
-        <span>Sat - Thu: 8AM - 7PM</span>
-      </li>
-    </ul>
-    <ul class="flex justify-between ms-auto md:ms-0 ">
-      <li>
-        <NuxtLink to="/" aria-label="facebook">
-          <Icon name="ri:facebook-fill" class="text-secondary mx-2 size-4" />
-        </NuxtLink>
-      </li>
-      <li>
-        <NuxtLink to="/" aria-label="twitter">
-          <Icon name="mdi:twitter" class="text-secondary mx-2 size-4" />
-        </NuxtLink>
-      </li>
-      <li>
-        <NuxtLink to="/" aria-label="linkedin">
-          <Icon name="basil:linkedin-solid" class="text-secondary mx-2 size-4" />
-        </NuxtLink>
-      </li>
-      <li>
-        <NuxtLink to="/" aria-label="google plus">
-          <Icon name="mdi:google-plus" class="text-secondary mx-2 size-4" />
-        </NuxtLink>
-      </li>
-    </ul>
+
+      <ul class="flex gap-5">
+        <li
+          v-for="item in contactInfo"
+          :key="item.label"
+          :class="item.hiddenOnMobile"
+          class="flex items-center justify-center gap-1"
+        >
+          <Icon
+            :name="item.icon"
+            class="text-secondary font-semibold"
+          />
+          <span>{{ item.label }}</span>
+        </li>
+      </ul>
+
+      <ul class="flex justify-between ms-auto md:ms-0">
+        <li
+          v-for="social in socialLinks"
+          :key="social.label"
+        >
+          <NuxtLink
+            :to="social.to"
+            :aria-label="social.label"
+          >
+            <Icon
+              :name="social.icon"
+              class="text-secondary mx-2 size-4"
+            />
+          </NuxtLink>
+        </li>
+      </ul>
+
     </div>
   </header>
 </template>
+
+<script setup>
+const contactInfo = [
+  {
+    icon: "lineicons:phone",
+    label: "1-234-567-8000",
+    hiddenOnMobile: "",
+  },
+  {
+    icon: "mdi-light:email",
+    label: "tranship@mail.com",
+    hiddenOnMobile: "hidden md:flex",
+  },
+  {
+    icon: "mdi-light:clock",
+    label: "Sat - Thu: 8AM - 7PM",
+    hiddenOnMobile: "hidden md:flex",
+  },
+]
+
+const socialLinks = [
+  {
+    icon: "ri:facebook-fill",
+    label: "facebook",
+    to: "/",
+  },
+  {
+    icon: "mdi:twitter",
+    label: "twitter",
+    to: "/",
+  },
+  {
+    icon: "basil:linkedin-solid",
+    label: "linkedin",
+    to: "/",
+  },
+  {
+    icon: "mdi:google-plus",
+    label: "google plus",
+    to: "/",
+  },
+]
+</script>
